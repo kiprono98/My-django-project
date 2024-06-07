bd82e78f508b76db1b6447c457a83e0574a1d84e
kiprono98 <kipronojosea@gmail.com>
2024-06-07 13:36:11 +0300

    I add a details file and updated the url

--- students/views.py
+++ students/views.py
@@ -2,9 +2,8 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import Student
 
-
 def students(request):
-  mystudent = Student.objects.all().values()
+  mystudents = Student.objects.all().values()
   template = loader.get_template('all_students.html')
   context = {
     'mystudents': mystudents,
